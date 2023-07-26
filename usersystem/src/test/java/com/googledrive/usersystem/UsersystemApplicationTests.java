e79d03ba1db9babd448a98f8a1cfe1b1a16bd00f
Lokesh118 <lokeshnelapati12@gmail.com>
2023-07-26 22:39:58 +0530

    register login added

--- usersystem/src/test/java/com/googledrive/usersystem/UsersystemApplicationTests.java
+++ usersystem/src/test/java/com/googledrive/usersystem/UsersystemApplicationTests.java
@@ -2,12 +2,15 @@ package com.googledrive.usersystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class UsersystemApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// assertThat(context).isNotNull();
 	}
 
 }
